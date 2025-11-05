24ba84d5f3d6544e18205b15883610bdf509f1db
Jakub Grątkiewicz <jakubgr@Mac.lan>
2025-11-05 18:33:31 +0100

    Database connection fixup

--- src/db/db.js
+++ src/db/db.js
@@ -2,17 +2,27 @@ import 'dotenv/config';
 import pkg from 'pg';
 const { Pool } = pkg;
 
-export const pool = new Pool({
+export const pool = process.env.SSLREQUIRE ?? false ?
+
+new Pool({
   host: process.env.PGHOST,
   port: Number(process.env.PGPORT ?? 5432),
   database: process.env.PGDATABASE,
   user: process.env.PGUSER,
   password: process.env.PGPASSWORD,
   ssl: {
-        rejectUnauthorized: process.env.SSLREQUIRE,
-        ca: process.env.SSLCERT,
+        rejectUnauthorized: process.env.SSLREQUIRE ?? false,
+        ca: process.env.SSLCERT ?? "",
     },
-});
+}) :
+new Pool({
+  host: process.env.PGHOST,
+  port: Number(process.env.PGPORT ?? 5432),
+  database: process.env.PGDATABASE,
+  user: process.env.PGUSER,
+  password: process.env.PGPASSWORD,
+}) 
+
 
 pool.on('error', (err) => {
   console.error('Błąd puli PG:', err);
